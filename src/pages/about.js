24b398c3d7fbbb1e2edb030157c3c481c4fb33fd
tahayasinkuloglu <steam_lg@outlook.com>
2023-06-01 22:40:37 +0300

    error fix

--- src/pages/about.js
+++ src/pages/about.js
@@ -50,18 +50,18 @@ const about = () => {
             <div className='col-span-3 flex flex-col items-start justify-start'>
               <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
               <p className='font-medium'>
-                Hi, I'm CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
+                Hi, Im CodeBucks, a web developer and UI/UX designer with a passion for creating beautiful, functional, 
                 and user-centered digital experiences. With 4 years of experience in the field. I am always looking for 
-                new and innovative ways to bring my clients' visions to life.
+                new and innovative ways to bring my clients visions to life.
               </p>
 
               <p className='my-4 font-medium'>
-                I believe that design is about more than just making things look pretty – it's about solving problems and 
+                I believe that design is about more than just making things look pretty – its about solving problems and 
                 creating intuitive, enjoyable experiences for users. 
               </p>
 
               <p className='font-medium'>
-                Whether I'm working on a website, mobile app, or 
+                Whether Im working on a website, mobile app, or 
                 other digital product, I bring my commitment to design excellence and user-centered thinking to 
                 every project I work on. I look forward to the opportunity to bring my skills and passion to your next project.
               </p>
